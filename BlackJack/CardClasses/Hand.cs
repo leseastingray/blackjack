23b6a19a3e61ec70517a7040aff59e9ccbd9b14d
Student <Student@1912851-C20251.lanecc.edu>
2019-04-09 15:42:45 -0700

    A beginning; most of Hand class is done, still need tests

--- BlackJack/CardClasses/Hand.cs
+++ BlackJack/CardClasses/Hand.cs
@@ -8,56 +8,108 @@ namespace CardClasses
 {
     public class Hand
     {
-
+        protected List<Card> handCards = new List<Card>();
+        // default constructor
         public Hand() { }
+        //overloaded constructor
         public Hand(Deck d, int numCards)
         {
+            for (int i = 0; i >= numCards; i++)
+            {
+                d.Deal();
+            }
         }
 
         public int NumCards
         {
             get
             {
-                return 0;
+                return handCards.Count;
             }
         }
 
         public void AddCard(Card c)
         {
-        }
+            for (int i = 1; i < handCards.Count; i++)
 
-        public Card GetCard(int index)
+            handCards.Add(c);
+        }
+        // indexer
+        public Card this[int i]
         {
-            return null;
+            get
+            {
+                return handCards[i];
+            }
         }
-
+        // index method
         public int IndexOf(Card c)
         {
+            foreach (Card cardInHand in handCards)
+            {
+                if (cardInHand == c)
+                {
+                    return handCards.IndexOf(c);
+                }
+            }
             return -1;
         }
 
         public int IndexOf(int value)
         {
+            foreach (Card c in handCards)
+            {
+                if (handCards.IndexOf(c) == value)
+                {
+                    return handCards.IndexOf(c);
+                }
+            }
             return -1;
         }
 
         public int IndexOf(int value, int suit)
         {
+            foreach (Card c in handCards)
+            {
+                if (c.Value == value)
+                {
+                    if (c.Suit == suit)
+                    {
+                        return handCards.IndexOf(c);
+                    }
+                }
+            }
             return -1;
         }
 
+        //important
         public bool HasCard(Card c)
         {
-            return false;
+            if (handCards.IndexOf(c) != -1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         public bool HasCard(int value)
         {
+            if (IndexOf(value) != -1)
+            {
+                return true;
+            }
             return false;
         }
 
         public bool HasCard(int value, int suit)
         {
+            if (IndexOf(value, suit) != -1)
+            {
+                return true;
+            }
             return false;
         }
 
