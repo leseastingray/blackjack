ba3607c39e7a274c3e0e511a2d45144559421205
Student <Student@1912851-C20251.lanecc.edu>
2019-04-11 14:29:55 -0700

    beginning to add HandTests

--- BlackJack/CardUnitTests/HandTests.cs
+++ BlackJack/CardUnitTests/HandTests.cs
@@ -8,7 +8,30 @@ using CardClasses;
 
 namespace CardUnitTests
 {
+    [TestFixture]
     public class HandTests
     {
+        [Test]
+        public void TestConstructor()
+        {
+            // declared and initialize Hand w/default constructor
+            Hand hand1 = new Hand();
+            // declared and initialize Card deck
+            Deck deck1 = new Deck();
+            Deck deck2 = new Deck();
+            // declared and initialize Hands w/overloaded constructor
+            Hand hand2 = new Hand(deck1, 5);
+            Hand hand3 = new Hand(deck2, 5);
+            // card at index 0 should be the same in hand2 and hand3
+            Assert.AreEqual(hand2.IndexOf(0), hand3.IndexOf(0));
+            // card at index 4 should be the same in hand2 and hand3
+            Assert.AreEqual(hand2.IndexOf(4), hand3.IndexOf(4));
+        }
+
+        [Test]
+        public void TestNumCards()
+        {
+
+        }
     }
 }
