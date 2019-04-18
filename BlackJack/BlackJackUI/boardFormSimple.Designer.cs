7d39e5a0d3d45915c592f619837be7d48b5fdf8b
Student <Student@1912851-C20251.lanecc.edu>
2019-04-18 16:12:18 -0700

    added blackjack UI

---
+++ BlackJack/BlackJackUI/boardFormSimple.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace BlackJack
+{
+    partial class boardFormSimple
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(boardForm));
+            this.card1 = new System.Windows.Forms.PictureBox();
+            this.card2 = new System.Windows.Forms.PictureBox();
+            this.card3 = new System.Windows.Forms.PictureBox();
+            this.card4 = new System.Windows.Forms.PictureBox();
+            this.card5 = new System.Windows.Forms.PictureBox();
+            this.card19 = new System.Windows.Forms.PictureBox();
+            this.card18 = new System.Windows.Forms.PictureBox();
+            this.card17 = new System.Windows.Forms.PictureBox();
+            this.card16 = new System.Windows.Forms.PictureBox();
+            this.card20 = new System.Windows.Forms.PictureBox();
+            this.hitButton = new System.Windows.Forms.Button();
+            this.standButton = new System.Windows.Forms.Button();
+            this.playerWinLabel = new System.Windows.Forms.Label();
+            this.dealerWinLabel = new System.Windows.Forms.Label();
+            this.playAgainButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.card1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card19)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card18)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card17)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card16)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card20)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // card1
+            // 
+            this.card1.Image = ((System.Drawing.Image)(resources.GetObject("card1.Image")));
+            this.card1.Location = new System.Drawing.Point(21, 18);
+            this.card1.Margin = new System.Windows.Forms.Padding(2);
+            this.card1.Name = "card1";
+            this.card1.Size = new System.Drawing.Size(82, 124);
+            this.card1.TabIndex = 0;
+            this.card1.TabStop = false;
+            // 
+            // card2
+            // 
+            this.card2.Image = ((System.Drawing.Image)(resources.GetObject("card2.Image")));
+            this.card2.Location = new System.Drawing.Point(121, 18);
+            this.card2.Margin = new System.Windows.Forms.Padding(2);
+            this.card2.Name = "card2";
+            this.card2.Size = new System.Drawing.Size(80, 124);
+            this.card2.TabIndex = 1;
+            this.card2.TabStop = false;
+            // 
+            // card3
+            // 
+            this.card3.Image = ((System.Drawing.Image)(resources.GetObject("card3.Image")));
+            this.card3.Location = new System.Drawing.Point(215, 18);
+            this.card3.Margin = new System.Windows.Forms.Padding(2);
+            this.card3.Name = "card3";
+            this.card3.Size = new System.Drawing.Size(81, 124);
+            this.card3.TabIndex = 2;
+            this.card3.TabStop = false;
+            // 
+            // card4
+            // 
+            this.card4.Image = ((System.Drawing.Image)(resources.GetObject("card4.Image")));
+            this.card4.Location = new System.Drawing.Point(312, 18);
+            this.card4.Margin = new System.Windows.Forms.Padding(2);
+            this.card4.Name = "card4";
+            this.card4.Size = new System.Drawing.Size(83, 124);
+            this.card4.TabIndex = 3;
+            this.card4.TabStop = false;
+            // 
+            // card5
+            // 
+            this.card5.Image = ((System.Drawing.Image)(resources.GetObject("card5.Image")));
+            this.card5.Location = new System.Drawing.Point(413, 18);
+            this.card5.Margin = new System.Windows.Forms.Padding(2);
+            this.card5.Name = "card5";
+            this.card5.Size = new System.Drawing.Size(84, 124);
+            this.card5.TabIndex = 4;
+            this.card5.TabStop = false;
+            // 
+            // card19
+            // 
+            this.card19.Image = ((System.Drawing.Image)(resources.GetObject("card19.Image")));
+            this.card19.Location = new System.Drawing.Point(316, 373);
+            this.card19.Margin = new System.Windows.Forms.Padding(2);
+            this.card19.Name = "card19";
+            this.card19.Size = new System.Drawing.Size(83, 123);
+            this.card19.TabIndex = 11;
+            this.card19.TabStop = false;
+            // 
+            // card18
+            // 
+            this.card18.Image = ((System.Drawing.Image)(resources.GetObject("card18.Image")));
+            this.card18.Location = new System.Drawing.Point(219, 373);
+            this.card18.Margin = new System.Windows.Forms.Padding(2);
+            this.card18.Name = "card18";
+            this.card18.Size = new System.Drawing.Size(81, 123);
+            this.card18.TabIndex = 10;
+            this.card18.TabStop = false;
+            // 
+            // card17
+            // 
+            this.card17.Image = ((System.Drawing.Image)(resources.GetObject("card17.Image")));
+            this.card17.Location = new System.Drawing.Point(121, 373);
+            this.card17.Margin = new System.Windows.Forms.Padding(2);
+            this.card17.Name = "card17";
+            this.card17.Size = new System.Drawing.Size(80, 123);
+            this.card17.TabIndex = 9;
+            this.card17.TabStop = false;
+            // 
+            // card16
+            // 
+            this.card16.Image = ((System.Drawing.Image)(resources.GetObject("card16.Image")));
+            this.card16.Location = new System.Drawing.Point(21, 373);
+            this.card16.Margin = new System.Windows.Forms.Padding(2);
+            this.card16.Name = "card16";
+            this.card16.Size = new System.Drawing.Size(82, 123);
+            this.card16.TabIndex = 8;
+            this.card16.TabStop = false;
+            // 
+            // card20
+            // 
+            this.card20.Image = ((System.Drawing.Image)(resources.GetObject("card20.Image")));
+            this.card20.Location = new System.Drawing.Point(413, 373);
+            this.card20.Margin = new System.Windows.Forms.Padding(2);
+            this.card20.Name = "card20";
+            this.card20.Size = new System.Drawing.Size(79, 123);
+            this.card20.TabIndex = 30;
+            this.card20.TabStop = false;
+            // 
+            // hitButton
+            // 
+            this.hitButton.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hitButton.Location = new System.Drawing.Point(21, 235);
+            this.hitButton.Name = "hitButton";
+            this.hitButton.Size = new System.Drawing.Size(103, 39);
+            this.hitButton.TabIndex = 53;
+            this.hitButton.Text = "Hit";
+            this.hitButton.UseVisualStyleBackColor = true;
+            this.hitButton.Click += new System.EventHandler(this.hitButton_Click);
+            // 
+            // standButton
+            // 
+            this.standButton.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.standButton.Location = new System.Drawing.Point(130, 235);
+            this.standButton.Name = "standButton";
+            this.standButton.Size = new System.Drawing.Size(103, 39);
+            this.standButton.TabIndex = 54;
+            this.standButton.Text = "Stand";
+            this.standButton.UseVisualStyleBackColor = true;
+            this.standButton.Click += new System.EventHandler(this.standButton_Click);
+            // 
+            // playerWinLabel
+            // 
+            this.playerWinLabel.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playerWinLabel.Location = new System.Drawing.Point(76, 168);
+            this.playerWinLabel.Name = "playerWinLabel";
+            this.playerWinLabel.Size = new System.Drawing.Size(100, 23);
+            this.playerWinLabel.TabIndex = 55;
+            this.playerWinLabel.Text = "Winner!";
+            this.playerWinLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // dealerWinLabel
+            // 
+            this.dealerWinLabel.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dealerWinLabel.Location = new System.Drawing.Point(76, 326);
+            this.dealerWinLabel.Name = "dealerWinLabel";
+            this.dealerWinLabel.Size = new System.Drawing.Size(100, 30);
+            this.dealerWinLabel.TabIndex = 56;
+            this.dealerWinLabel.Text = "Winner!";
+            this.dealerWinLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // playAgainButton
+            // 
+            this.playAgainButton.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playAgainButton.Location = new System.Drawing.Point(312, 235);
+            this.playAgainButton.Name = "playAgainButton";
+            this.playAgainButton.Size = new System.Drawing.Size(180, 39);
+            this.playAgainButton.TabIndex = 57;
+            this.playAgainButton.Text = "Play Again";
+            this.playAgainButton.UseVisualStyleBackColor = true;
+            this.playAgainButton.Click += new System.EventHandler(this.playAgainButton_Click);
+            // 
+            // boardForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGreen;
+            this.ClientSize = new System.Drawing.Size(516, 525);
+            this.Controls.Add(this.playAgainButton);
+            this.Controls.Add(this.dealerWinLabel);
+            this.Controls.Add(this.playerWinLabel);
+            this.Controls.Add(this.standButton);
+            this.Controls.Add(this.hitButton);
+            this.Controls.Add(this.card20);
+            this.Controls.Add(this.card19);
+            this.Controls.Add(this.card18);
+            this.Controls.Add(this.card17);
+            this.Controls.Add(this.card16);
+            this.Controls.Add(this.card5);
+            this.Controls.Add(this.card4);
+            this.Controls.Add(this.card3);
+            this.Controls.Add(this.card2);
+            this.Controls.Add(this.card1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "boardForm";
+            this.Text = "Let\'s Play BlackJack";
+            this.Load += new System.EventHandler(this.frmBoard_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.card1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card19)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card18)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card17)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card16)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card20)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox card1;
+        private System.Windows.Forms.PictureBox card2;
+        private System.Windows.Forms.PictureBox card3;
+        private System.Windows.Forms.PictureBox card4;
+        private System.Windows.Forms.PictureBox card5;
+        private System.Windows.Forms.PictureBox card19;
+        private System.Windows.Forms.PictureBox card18;
+        private System.Windows.Forms.PictureBox card17;
+        private System.Windows.Forms.PictureBox card16;
+        private System.Windows.Forms.PictureBox card20;
+        private System.Windows.Forms.Button hitButton;
+        private System.Windows.Forms.Button standButton;
+        private System.Windows.Forms.Label playerWinLabel;
+        private System.Windows.Forms.Label dealerWinLabel;
+        private System.Windows.Forms.Button playAgainButton;
+    }
+}
+
---
+++ BlackJack/BlackJackUI/boardFormSimple.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using CardClasses;
+
+namespace BlackJack
+{
+    public partial class boardFormSimple : Form
+    {
+        #region Instance Variables
+        #endregion
+
+        public boardFormSimple()
+        {
+            InitializeComponent();
+        }
+
+        #region Methods
+        public void Show(PictureBox p, Card c)
+        {
+            p.Image = Image.FromFile(System.Environment.CurrentDirectory
+                + "\\Cards\\" + c.FileName);
+        }
+
+        public void ShowBack(PictureBox p, Card c)
+        {
+            p.Image = Image.FromFile(System.Environment.CurrentDirectory
+                + "\\Cards\\black_back.jpg");
+        }
+        #endregion
+
+        private void frmBoard_Load(object sender, EventArgs e)
+        {
+            hitButton.Enabled = true;
+            standButton.Enabled = true;
+            playerWinLabel.Visible = false;
+            dealerWinLabel.Visible = false;
+            playAgainButton.Enabled = false;
+        }
+
+        private void hitButton_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void standButton_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void playAgainButton_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
