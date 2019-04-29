72123800caf1669592fabc0a542afee153e1c849
alicia1206 <Student@1913511-C18096.lanecc.edu>
2019-04-29 16:28:33 -0700

    added more comments

--- BlackJack/BlackJackUI/boardFormSimple.cs
+++ BlackJack/BlackJackUI/boardFormSimple.cs
@@ -21,12 +21,6 @@ namespace BlackJack
         private int startUPicBoxNumber = 18;
         private int startCPicBoxNumber = 3;
 
-        private int userSum;
-        private int compSum;
-
-        private int turn = -1;
-        private const int USER = 1;
-        private const int COMPUTER = 0;
         #endregion
 
         public boardFormSimple()
@@ -49,23 +43,28 @@ namespace BlackJack
 
         public void DealCards()
         {
+            // shuffle deck
             cDeck.Shuffle();
 
+            // deal 1st card to user, show in pic box
             Card userCard1 = cDeck.Deal();
             userHand.AddCard(userCard1);
             card16.Visible = true;
             Show(card16, userCard1);
 
+            // deal 2nd card to user, show in pic box
             Card userCard2 = cDeck.Deal();
             userHand.AddCard(userCard2);
             card17.Visible = true;
             Show(card17, userCard2);
 
+            // deal 1st card to computer, show in pic box
             Card compCard1 = cDeck.Deal();
             compHand.AddCard(compCard1);
             card1.Visible = true;
             ShowBack(card1, compCard1);
 
+            // deal 2nd card to computer, show in pic box
             Card compCard2 = cDeck.Deal();
             compHand.AddCard(compCard2);
             card2.Visible = true;
@@ -84,12 +83,15 @@ namespace BlackJack
                 return;
             }
 
+            // deal 1 card and add it to userHand
             Card hitCard = cDeck.Deal();
             userHand.AddCard(hitCard);
+            // show above card in appropriate pic box
             PictureBox hit = (PictureBox)this.Controls["card" + startUPicBoxNumber];
             hit.Visible = true;
             Show(hit, hitCard);
 
+            // if user busts
             if (userHand.IsBusted == true)
             {
                 // computer shows cards
@@ -122,22 +124,21 @@ namespace BlackJack
             compHit.Visible = true;
             // show back of card in compHit pic box
             Show(compHit, compCard);
-
-            // let user play again
-
         }
 
         public void Stand()
         {
+            // while computer has pic box space and has score below 18
             while (startCPicBoxNumber < 6 && compHand.Score < 18)
             {
                 MakeComputerMove();
             }
-
+            // call check score method
             CheckScore();
         }
         public void CheckScore()
         {
+            // if computer busts
             if (compHand.IsBusted == true)
             {
                 // user wins!
@@ -154,6 +155,7 @@ namespace BlackJack
                 standButton.Enabled = false;
                 playAgainButton.Enabled = true;
             }
+            // else if userHand score is greater than compHand score
             else if (userHand.Score > compHand.Score)
             {
                 // user wins!
@@ -171,6 +173,7 @@ namespace BlackJack
                 standButton.Enabled = false;
                 playAgainButton.Enabled = true;
             }
+            // else if compHand score is greater than userHand score
             else if (compHand.Score > userHand.Score)
             {
                 // computer wins!
@@ -187,6 +190,7 @@ namespace BlackJack
                 standButton.Enabled = false;
                 playAgainButton.Enabled = true;
             }
+            // else if scores are equal
             else if (compHand.Score == userHand.Score)
             {
                 // it's a tie!
@@ -205,26 +209,28 @@ namespace BlackJack
                 playAgainButton.Enabled = true;
             }
         }
+        // sets up new game
         public void NewGame()
         {
+            // clear userHand of cards
             while (userHand.NumCards > 0)
             {
                 userHand.Discard(0);
             }
-
+            // clear compHand of cards
             while (compHand.NumCards > 0)
             {
                 compHand.Discard(0);
             }
 
-            // hide computer hand picture boxes
-            for (int i = 1; i < 6; i++)
+            // hide user hand picture boxes
+            for (int i = 16; i < 21; i++)
             {
                 this.Controls["card" + i].Visible = false;
             }
 
-            // hide user hand picture boxes
-            for (int i = 16; i < 21; i++)
+            // hide computer hand picture boxes
+            for (int i = 1; i < 6; i++)
             {
                 this.Controls["card" + i].Visible = false;
             }
